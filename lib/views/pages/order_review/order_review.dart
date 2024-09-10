4a7dc16995184cabdeb407fa1cef5b5b02d77c19
S <shakibmohammad18@gmail.com>
2024-09-10 19:00:51 +0600

    getx implement

--- lib/views/pages/order_review/order_review.dart
+++ lib/views/pages/order_review/order_review.dart
@@ -170,7 +170,6 @@ class _OrderReviewPageState extends State<OrderReviewPage> {
                     ],
                   ),
                 ),
-
                 const SizedBox(
                   height: 10,
                 ),
